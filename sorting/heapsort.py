43fb56fde6eb2c65d353320ecc96cfe7c44c0ac5
adityapalve <70425922+adityapalve@users.noreply.github.com>
2023-08-16 19:02:34 +0100

    cleaning

--- sorting/heapsort.py
+++ sorting/heapsort.py
@@ -1,12 +1,1 @@
 # HeapSort
-def duplicateZeros(arr):
-    l = len(arr)
-    zeroes = arr.count(0)
-    print(zeroes)
-    for i in range(l-1,-1,-1):
-        print(i)
-
-    return 0
-
-a = [1,0,2,3,0,4,5,0]
-duplicateZeros(a)
