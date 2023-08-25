436617700312e843c7edb717b3a0357fb07ba23a
adityapalve <70425922+adityapalve@users.noreply.github.com>
2023-08-25 23:13:22 +0100

    bs

--- sorting/heapsort.py
+++ sorting/heapsort.py
@@ -1,1 +1,28 @@
 # HeapSort
+def merge(left, right):
+    l,r = 0,0 
+    output = []
+
+    while l< len(left) and r< len(right): 
+      if left[l]<right[r]:
+          output.append(left[l])
+          l+=1
+      else:
+          output.append(right[r])          
+          r+=1
+    output.extend(left[l:])
+    output.extend(right[r:])
+    return output
+
+def mergesort(nums: list) -> list:
+    if len(nums)<=1:
+        return []
+
+    mid = len(nums)//2
+    
+    left  = mergesort(nums[:mid])
+    right = mergesort(nums[mid:])
+    return merge(left, right)
+
+a = [3,5,24,6]
+print(mergesort(a))
