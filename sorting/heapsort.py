392125edc493f030c10980c0239af62844d9c68d
adityapalve <70425922+adityapalve@users.noreply.github.com>
2023-08-28 23:14:02 +0100

    some practice

--- sorting/heapsort.py
+++ sorting/heapsort.py
@@ -1,35 +1,18 @@
 # HeapSort
-def merge(left, right):
-    output = []
-    l, r = 0, 0
+def quicksort(nums):
+    left, right, equals = [], [], []
+    if(len(nums)<1):
+        return []
+    mid = len(nums)//2
 
-    while l < len(left) and r < len(right):
-        if left[l] < right[r]:
-            output.append(left[l])
-            l += 1
+    for i in nums:
+        if i< nums[mid]:
+            left.append(i)
+        elif i > nums[mid]:
+            right.append(i)
         else:
-            output.append(right[r])
-            r += 1
-    output.extend(left[l:])
-    output.extend(right[r:])
-    return output
+            equals.append(i)
 
-
-def mergesort(nums: list) -> list:
-    if len(nums) <= 1:
-        return nums  
-
-    mid = len(nums) // 2
-
-    left = mergesort(nums[:mid])
-    print(left)
-    right = mergesort(nums[mid:])
-    print(right)
-    return merge(left,right) 
-
+    return quicksort(left) + equals + quicksort(right)
 a = [3, 5, 24, 6]
-# mid = len(a) // 2
-# print(a[:mid])
-# print(a[mid:])
-# print(merge(a[:mid], [6,24]))
-print(mergesort(a))
+print(quicksort(a))
