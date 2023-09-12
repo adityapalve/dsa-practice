97999ca257f51d3429ad432d341ede9d3ab5c856
adityapalve <70425922+adityapalve@users.noreply.github.com>
2023-09-12 14:49:38 +0100

    kv-store problem

---
+++ bit.py
@@ -0,0 +1,5 @@
+# Figuring out some of the bitwise operations and how they work.
+
+def main():
+  pass
+
---
+++ lcs/kv-store.py
@@ -0,0 +1,10 @@
+class TimeMap:
+  def __init__(self):
+    self.val = []
+  
+  def set(self, key:str, value:int, timestamp:int):
+    pass
+  
+  def get(self, key:str, timestamp:int):
+    pass
+
