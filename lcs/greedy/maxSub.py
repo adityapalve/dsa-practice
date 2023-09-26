9daeebaea739c8b513ca453e83d9516c48bcc16a
adityapalve <70425922+adityapalve@users.noreply.github.com>
2023-09-26 20:43:39 +0100

    optimal solution

--- lcs/greedy/maxSub.py
+++ lcs/greedy/maxSub.py
@@ -10,11 +10,22 @@ def maxSubarry(nums:list[int]) -> int:
   return maxSum
 """
 def maxSubarray(nums:list[int]) -> int:
-  
-  return 0
+  maxSum = nums[0] 
+  currSum = 0
+  for i in nums:
+    if currSum<0:
+      currSum = 0
+    currSum = currSum+i
+    maxSum = max(maxSum, currSum)
+  return maxSum
+ 
 
 nums = [-2,1,-3,4,-1,2,1,-5,4]
 output = 6
-if maxSubarray(nums)== output:
-  print('Test Passed', maxSubarray(nums), output)
+print(maxSubarray(nums))
+
+# if maxSubarray(nums)== output:
+#   print('Test Passed', maxSubarray(nums), output)
+# else:
+#   print("FAILED")
 
