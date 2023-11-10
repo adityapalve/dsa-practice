69f3634a4d9fabadf7acd84757b37c59c68cdc2c
adityapalve <aditya4palve@gmail.com>
2023-11-10 01:08:39 +0000

    added alternative method

--- lcs/arrays/product_except_self.py
+++ lcs/arrays/product_except_self.py
@@ -19,6 +19,23 @@ for i, e in enumerate(l[::-1]):
     out[len(l)-i-1] *= rp
 print(out)
 
+"""
+Alternative Method:
+
+3 for loops
+prefix = [1]*n
+        suffix = [1]*n
+        ans = [1]*n
+        for i in range(1,len(nums)):
+            prefix[i] = prefix[i-1]*nums[i-1]
+        
+        for i in range(n-2, -1, -1):
+            suffix[i] = suffix[i+1]*nums[i+1]
+        
+        for i in range(n):
+            ans[i] = prefix[i]*suffix[i]
+"""
+
 '''
 0 3
 1 2
