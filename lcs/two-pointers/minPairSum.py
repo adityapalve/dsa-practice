4631f503046ab89a17e9996317d436e03fe7c741
adityapalve <aditya4palve@gmail.com>
2023-11-17 17:14:57 +0000

    solved Min Pair Sum two pointer + greedy

---
+++ lcs/two-pointers/minPairSum.py
@@ -0,0 +1,16 @@
+def minPairSum(nums):
+  nums.sort()
+  lp, rp =  0, len(nums)-1
+  max_sum = 0
+  pairs = []
+  while lp<rp:
+      pairs.append((nums[lp],nums[rp]))
+      lp += 1
+      rp -= 1
+  
+  sums = [sum(s) for s in pairs]
+  return max(sums)
+
+nums= [3,5,4,2,4,6]
+output = 8
+print(minPairSum(nums))
